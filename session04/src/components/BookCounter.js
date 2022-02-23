4675f23c8c3665ef0c81dca117c18c434663f12c
sangeunyoon <yse1468@gmail.com>
2022-02-23 22:04:03 +0000

    Did some Jason

--- session04/src/components/BookCounter.js
+++ session04/src/components/BookCounter.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
+import "./BookCounter.css";
+
 
 const BookCounter = (props) => {
-
   return (
     <main>
       <h2>{props.library.name}'s Books</h2>
-      {props.library.books.map((book) => (<div>{book.title}</div>))}
+      <button onClick={() => props.refreshBooks()}>Refresh Books</button>
+      <div className="booklist">
+       <ul>
+        {props.library.books.map((book) => (
+          <li>
+            <h3>{book.volumeInfo.title}</h3>
+            <img src={book.volumeInfo.imageLinks.thumbnail}/>
+            <p> Description: {book.volumeInfo.description}</p> 
+         <ul>
+           {book.volumeInfo.authors.map((author) =>(
+           <li>{author}</li>
+           ))}
+         </ul>
+          <div>price: £ {book.saleInfo.retailPrice && book.saleInfo.retailPrice.amount}</div>
+
+          <div>Average Ratings : {book.volumeInfo.averageRating}</div>
+          </li>
+        ))}
+      </ul>
+      </div>
     </main>
+
   );
-}
+};
 
 export default BookCounter;
--- session04/src/index.js
+++ session04/src/index.js
@@ -1,17 +1,22 @@
 import React, { useState, Fragment } from 'react';
 import ReactDOM from 'react-dom';
-//import bookArray from './models/books.json';
+import bookArray from './models/books.json';
 
 import BookCounter from './components/BookCounter';
 
 const App = () => {
-  const bookArray =  [{"title":"Half of a Yellow Sun"},{"title":"Born a Crime"},{"title":"Americanah"},{"title":"Ghana Must Go"}];
+  // const bookArray =  [{"title":"Half of a Yellow Sun"},{"title":"Born a Crime"},{"title":"Americanah"},{"title":"Ghana Must Go"}];
   const [localLibrary,setLibrary] = useState({name:"Sarah", books : bookArray});
 
+  const refreshBooks = () => {
+    setLibrary({
+      name:"Sarah", books : [{
+       "volumeInfo":{
+        "title":"The Wind Blew West"}}]});
+  }
   return <Fragment>
     <h1>Welcome to My Library</h1>
-    <BookCounter library={localLibrary}/>
+    <BookCounter library={localLibrary}  refreshBooks={refreshBooks}/>
   </Fragment>;
 }
-
 ReactDOM.render(<App/>,document.getElementById('root'));
