38a4be47265b1aa33e7ef8f72bc9a3894ba429c2
sangeunyoon <yse1468@gmail.com>
2022-03-07 21:02:37 +0000

    put some submitting form

--- session07/src/index.js
+++ session07/src/index.js
@@ -1,31 +1,79 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+//This is CSS import //
+import "../src/style.css";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const LogForm = () => {
-  const [name, setName] = useState('');
-  const [,setSubmitted] = useState(false);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [location, setLocation] = useState("");
+  const [hasSubmitted, setSubmitted] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
     setSubmitted(true);
   }
-
   return (
-    <div>
-      <h1>Your Details</h1>
-      <h2>Please fill in the form details</h2>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <div>
-          <label>Name:<input type="text" value={name} onChange={(e) => { setName(e.target.value); setSubmitted(false) }} /></label>
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
+    <>
       <div>
-        <h2>Results</h2>
-            Name: {name}<br />
-      </div>
-    </div>
-  );
-}
+        <h1>Your Details</h1>
+        <h2>Please fill in the form details</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label>
+              Name:
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                  setSubmitted(false);
+                }}
+              />
+            </label>
+            <br />
+            <label>
+              Age:
+              <input
+                type="number"
+                value={age}
+                onChange={(e) => {
+                  setAge(e.target.value);
+                  setSubmitted(false);
+                }}
+              />
+            </label>
+            <br />
+            <label>
+              Location:
+              <input
+                type="text"
+                value={location}
+                onChange={(e) => {
+                  setLocation(e.target.value);
+                  setSubmitted(false);
+                }}
+              />
+            </label>
+            <br />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+        <div>
+          <h2>Results</h2>
+          Name: {name}
+          <br />
+          Age: {age}
+          <br />
+          Location: {location}
+          <br />
+      
 
-ReactDOM.render(<LogForm />, document.getElementById('root'));
+        </div>
+        {hasSubmitted && <p>Thank you for submitting the form</p>}
+      </div>
+    </>
+  );
+};
+
+ReactDOM.render(<LogForm />, document.getElementById("root"));
